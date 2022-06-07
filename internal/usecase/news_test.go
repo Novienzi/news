f6f48d05be46608c2f2ef818658a6089db59ed26
Novien Ghoziana Indanartha <novienghi@gmail.com>
2022-06-07 14:09:51 +0700

    feature : news | unitest on usecase

--- internal/usecase/news_test.go
+++ internal/usecase/news_test.go
@@ -2,12 +2,32 @@ package usecase
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"news/internal/models"
 	"news/internal/models/mocks"
+	"strconv"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	ctx           = context.Background()
+	newsID        = 1
+	newsAuthor    = "vien"
+	newsTitle     = "Gunung Api Meletus"
+	newsBody      = "Pada hari senin, Gunung Api Purba di daerah z meletus dahsyat"
+	newsCreatedAt = time.Now()
+
+	news = models.News{
+		ID:        newsID,
+		Author:    newsAuthor,
+		Title:     newsTitle,
+		Body:      newsBody,
+		CreatedAt: newsCreatedAt,
+	}
 )
 
 func TestCreate(t *testing.T) {
@@ -18,23 +38,6 @@ func TestCreate(t *testing.T) {
 		mdl:     mdlMock,
 		rdsRepo: rdsMock,
 	}
-
-	var (
-		ctx           = context.Background()
-		newsID        = 1
-		newsAuthor    = "vien"
-		newsTitle     = "Gunung Api Meletus"
-		newsBody      = "Pada hari senin, Gunung Api Purba di daerah z meletus dahsyat"
-		newsCreatedAt = time.Now()
-
-		news = models.News{
-			ID:        newsID,
-			Author:    newsAuthor,
-			Title:     newsTitle,
-			Body:      newsBody,
-			CreatedAt: newsCreatedAt,
-		}
-	)
 
 	t.Run("success case", func(t *testing.T) {
 		mdlMock.On("Create", ctx, models.News{
@@ -50,5 +53,75 @@ func TestCreate(t *testing.T) {
 		})
 		assert.NoError(t, err)
 		assert.Equal(t, news, res)
+	})
+
+	t.Run("error case", func(t *testing.T) {
+		mdlMock.On("Create", ctx, models.News{
+			Author: newsAuthor,
+			Title:  newsTitle,
+			Body:   newsBody,
+		}).Return(models.News{}, sql.ErrNoRows).Once()
+
+		_, err := uc.Create(ctx, models.News{
+			Author: newsAuthor,
+			Title:  newsTitle,
+			Body:   newsBody,
+		})
+		assert.Error(t, err)
+	})
+}
+
+func TestGetNewsByID(t *testing.T) {
+	mdlMock := &mocks.NewsRepository{}
+	rdsMock := &mocks.RedisRepository{}
+
+	uc := newsUc{
+		mdl:     mdlMock,
+		rdsRepo: rdsMock,
+	}
+
+	t.Run("success case from db", func(t *testing.T) {
+		rdsMock.On("GetNewsByIDCtx", ctx, strconv.Itoa(newsID)).Return(nil, nil).Once()
+		mdlMock.On("GetNewsByID", ctx, newsID).Return(&news, nil)
+		rdsMock.On("SetNewsCtx", ctx, strconv.Itoa(newsID), 3600, &news).Return(nil).Once()
+
+		res, err := uc.GetNewsByID(ctx, newsID)
+		assert.NoError(t, err)
+		assert.Equal(t, &news, res)
+	})
+
+	t.Run("success case from redis", func(t *testing.T) {
+		rdsMock.On("GetNewsByIDCtx", ctx, strconv.Itoa(newsID)).Return(&news, nil).Once()
+
+		res, err := uc.GetNewsByID(ctx, newsID)
+		assert.NoError(t, err)
+		assert.Equal(t, &news, res)
+	})
+
+	t.Run("error case", func(t *testing.T) {
+		rdsMock.On("GetNewsByIDCtx", ctx, strconv.Itoa(newsID)).Return(nil, nil).Once()
+		mdlMock.On("GetNewsByID", ctx, newsID).Return(nil, sql.ErrNoRows).Once()
+		rdsMock.On("SetNewsCtx", ctx, strconv.Itoa(newsID), 3600, &news).Return(errors.New("error")).Once()
+
+		_, err := uc.GetNewsByID(ctx, newsID)
+		assert.Error(t, err)
+	})
+}
+
+func TestGetNews(t *testing.T) {
+	mdlMock := &mocks.NewsRepository{}
+	rdsMock := &mocks.RedisRepository{}
+
+	uc := newsUc{
+		mdl:     mdlMock,
+		rdsRepo: rdsMock,
+	}
+
+	t.Run("success", func(t *testing.T) {
+		mdlMock.On("GetNews", ctx, 10, 0, "keyword", "keyword").Return([]models.News{news}, nil)
+
+		res, _, err := uc.GetNews(ctx, 10, 0, "keyword", "keyword")
+		assert.NoError(t, err)
+		assert.Equal(t, &[]models.News{news}, res)
 	})
 }
